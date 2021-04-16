8fba5a3a56d1f6b7257ed6646961c9d13a061f04
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2021-04-16 16:46:32 +0200

    Update executor script

--- benchmarker.sh
+++ benchmarker.sh
@@ -8,15 +8,15 @@ echo "Start Benchmarks"
 echo "*********"
 echo "----------"
 echo "JPMML"
-cd ./jpmml-benchmarks
+cd ./jpmml-benchmark
 java -jar target/jpmml-benchmarks.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -rf json -rff jpmml-results.json SimpleLinearRegressionBenchmark
 echo "----------"
 echo "TRUSTY - NO DROOLS"
-cd ../trusty-nodrools-benchmarks
+cd ../trusty-nodrools-benchmark
 java -jar target/trusty-nodrools-benchmark.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -rf json -rff trusty-nodrools-results.json SimpleLinearRegressionBenchmark
 echo "----------"
 echo "TRUSTY - DROOLS"
-cd ../trusty-drools-benchmarks
+cd ../trusty-drools-benchmark
 java -jar target/trusty-drools-benchmarks.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -rf json -rff trusty-drools-results.json SimpleLinearRegressionBenchmark
 echo "----------"
 echo "*********"
